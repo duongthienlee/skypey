fb5cc5258e56ea574f049d805de32b989888820e
duongthienlee <duongthienlee@gmail.com>
2018-10-19 16:00:28 +0300

    first version, followed by Tutorial

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,19 @@
 import React from "react";
 import "./Main.css";
+import ChatWindow from "./ChatWindow"
+import Empty from "./Empty"
+const Main = ({ user, activeUserId }) => {
+    const renderMainContent = () => {
+        return !activeUserId ? <Empty user={user} activeUserId={activeUserId} /> : <ChatWindow activeUserId={activeUserId} />
 
-const Main = () => {
-    return <main className="Main">Main Stuff</main>;
+    }
+
+    return (
+        <main className="Main">
+            {renderMainContent()}
+        </main>
+    );
 };
 
 export default Main;
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from "redux";
 import user from "./user";
 import contacts from "./contacts";
-
+import activeUserId from "./activeUserId"
+import messages from "./messages"
+import typing from "./typing"
 export default combineReducers({
     user,
-    contacts
+    contacts,
+    activeUserId,
+    messages,
+    typing
 });
